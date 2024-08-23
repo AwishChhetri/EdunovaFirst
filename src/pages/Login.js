8d8624b70621e2765de1dbd25b5873d0979ceaa7
Abish Chhetri <95292407+AwishChhetri@users.noreply.github.com>
2024-08-23 21:26:50 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -45,6 +45,7 @@ const Login = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
+     <h1>Email: sneha@gmail.com, Password: D@rjeeling6507</h1>
       <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
         <h1 className="text-2xl font-semibold text-gray-900 mb-6">Login</h1>
         <form onSubmit={handleSubmit}>
